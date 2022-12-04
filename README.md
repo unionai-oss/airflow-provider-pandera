8990e7d60600df5cc1f13193d9b3384564ef2fba
erichamers <erichamers@gmail.com>
2022-12-04 17:05:04 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Apache Airflow Provider for Pandera
+
+A set of Airflow operators for [Pandera](https://github.com/unionai-oss/pandera).
---
+++ setup.py
@@ -0,0 +1,46 @@
+"""Setup.py for the pandera provider package."""
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="airflow-provider-sample",
+    version="0.0.1",
+    description="A sample provider package built by Astronomer.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "apache_airflow_provider": [
+            "provider_info=sample_provider.__init__:get_provider_info"
+        ]
+    },
+    license="Apache License 2.0",
+    packages=find_packages(exclude="tests*"),
+    install_requires=[
+        "apache-airflow>=2.0",
+        "pandera>=0.13.4",
+    ],
+    setup_requires=["setuptools", "wheel"],
+    author="Eric Hamers",
+    author_email="erichamers@gmail.com",
+    url="https://www.union.ai/pandera",
+    classifiers=[
+        "Framework :: Apache Airflow",
+        "Framework :: Apache Airflow :: Provider",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires="~=3.7",
+)
